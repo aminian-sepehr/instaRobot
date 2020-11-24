486489d47d40bd6044f6a899c6e1f3f101c2b85c
aminian-sepehr <aminian.sepehr@ut.ac.ir>
2020-11-24 14:08:50 +0330

    first commit

---
+++ instaRobot.py
@@ -0,0 +1,32 @@
+import emoji
+
+
+class robot:
+
+    def __init__(self):
+        pass
+
+
+    def follow_this_list(self):
+        pass
+
+    def unfollow_list(self):
+        pass
+
+    def get_users_info(self):
+        pass
+    def like_all_photos(self):
+        pass
+
+    def commnet_on_post(self, comment = emoji.emojize(':red_heart: :red_heart:')):
+        pass
+
+
+
+
+
+
+
+
+
+
