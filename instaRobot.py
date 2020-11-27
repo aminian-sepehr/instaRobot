b34fdde5956c049b813fb23799ead2c341c3ca87
aminian-sepehr <aminian.sepehr@ut.ac.ir>
2020-11-27 16:07:49 +0330

    seme methods developed

--- instaRobot.py
+++ instaRobot.py
@@ -6,9 +6,9 @@ import os
 
 class robot:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password1):
         self.user = username
-        self.passwd = password
+        self.passwd = password1
 
         self.session = InstaPy(username=self.user,
                                password=self.passwd,
@@ -20,7 +20,7 @@ class robot:
         self.session.set_relationship_bounds(enabled=False)
 
     def __follow_this_list(self, filepath):
-        if(os.path.isfile(filepath)):  # check if exists do this and else : follow a username
+        if (os.path.isfile(filepath)):  # check if exists do this and else : follow a username
             l1 = self.__readfile(filepath)
         else:
             l1 = list(filepath)
@@ -29,15 +29,29 @@ class robot:
         except(Exception):
             print('failed to follow\n')
 
-
-    def __unfollow_list(self, flist):
-        pass
+    def __unfollow_list(self, filepath):
+        if (os.path.isfile(filepath)):  # check if exists do this and else : follow a username
+            l1 = self.__readfile(filepath)
+        else:
+            l1 = list(filepath)
+        try:
+            self.session.unfollow_users(amount=200,
+                                        custom_list_enabled=True,
+                                        custom_list=l1,
+                                        custom_list_param="all",
+                                        style="RANDOM")
+        except(Exception):
+            print('failed to follow\n')
 
     def __get_users_info(self):
         pass
 
-    def __like_all_photos(self):
-        pass
+    def __like_all_photos(self,filepath):
+        self.session.set_user_interact()
+        self.session.like_by_feed(amount=200,
+                                  randomize=True,
+                                  unfollow=True,
+                                  interact=True)
 
     def __commnet_on_post(self, comment=emoji.emojize(':red_heart: :red_heart:')):
         pass
@@ -46,16 +60,17 @@ class robot:
         with smart_run(self.session):
             self.__config()
 
-            if(flag == 1):
+            if flag == 1:
                 self.__follow_this_list(filepath)
-            elif(flag == 2):
-                self.__unfollow_list()
-            elif(flag == 3):
-                self.__like_all_photos()
-            elif(flag == 4):
+            elif flag == 2:
+                self.__unfollow_list(filepath)
+            elif flag == 3:
+                self.__like_all_photos(filepath)
+            elif flag == 4:
                 self.__commnet_on_post()
             else:
                 raise 'flag error : flag number dowsnt match1 \n'
+
     def __readfile(self, filename):
         namelist = []
         try:
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import instaRobot
 
-username = "sara.bot.by.maral"
-passwd = "123sara456maral"
+username = ''
+passwd = ''
 robot = instaRobot.robot(username, passwd)
 working = True
 print("Welcome to insta robot what do you want to do?\n"
@@ -17,13 +17,53 @@ while (working):
     com_splited = command.split()
     if com_splited[0] == 'follow':
         # robot1.follow_this_list(com_splited[1])
-        robot.run(1,com_splited[1])
+        try:
+            robot.run(1,com_splited[1])
+        except(IndexError):
+            print('follow command must at least have an additional argument\n')
 
     elif com_splited[0] == 'unfollow':
-        pass
+        try:
+            robot.run(2, com_splited[1])
+        except(IndexError):
+            print(' Unfollow command must at least have an additional argument\n')
 
     elif com_splited[0] == 'exit':
         working = False
 
     else:
         print("wrong command\n")
+from instapy import InstaPy
+from instapy import smart_run
+
+# login credentials
+# insta_username = "sara.bot.by.maral"
+# insta_password = "123sara456maral"
+# # insta_username = ''
+# # insta_password = ''
+#
+# # get an InstaPy session!
+# # set headless_browser=True to run InstaPy in the background
+# session = InstaPy(username=insta_username,
+#                   password=insta_password,
+#                   headless_browser=False)
+#
+# # with smart_run(session):
+# #     """ Activity flow """
+# #     # general settings
+# #     session.set_relationship_bounds(enabled=True,
+# #                                     delimit_by_numbers=True,
+# #                                     max_followers=4590,
+# #                                     min_followers=45,
+# #                                     min_following=77)
+# #
+# #     session.set_dont_include(["friend1", "friend2", "friend3"])
+# #     session.set_dont_like(["pizza", "#store"])
+# #
+# #     # activities
+# #
+# #     """ Massive Follow of users followers (I suggest to follow not less than
+# #     3500/4000 users for better results)...
+# #     """
+# #     session.follow_user_followers(['user1', 'user2', 'user3'], amount=800,
+# #                                   randomize=False, interact=False)
